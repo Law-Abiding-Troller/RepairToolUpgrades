349eb18ce05c66459e74ce10a4e3243f046e7c5f
Law Abiding Coder <colton.trash5.5@proton.me>
2025-06-25 18:10:30 -0500

    Build.

--- obj/Debug/net472/RepairToolUpgrades.AssemblyInfo.cs
+++ obj/Debug/net472/RepairToolUpgrades.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RepairToolUpgrades")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+560a1570208513a24fc1c20f769eb1321b5cdee4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+572cdc6027633d93ea7ae5cd9bb88f31e2c37c73")]
 [assembly: System.Reflection.AssemblyProductAttribute("RepairToolUpgrades")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RepairToolUpgrades")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
