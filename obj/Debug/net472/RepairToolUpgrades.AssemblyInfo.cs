e546a539f5f18933db231d4b452b12aba13ad99e
Law Abiding Coder <colton.trash5.5@proton.me>
2025-06-26 20:10:08 -0500

    Actually doubled the base repair speed

--- obj/Debug/net472/RepairToolUpgrades.AssemblyInfo.cs
+++ obj/Debug/net472/RepairToolUpgrades.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RepairToolUpgrades")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3479537ff3bb816e4c5a8d13d97fb68dd5eca10")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d82628c9e664418ab354dcfb6031b13c59b93409")]
 [assembly: System.Reflection.AssemblyProductAttribute("RepairToolUpgrades")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RepairToolUpgrades")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
